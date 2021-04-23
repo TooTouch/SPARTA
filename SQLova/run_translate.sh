ef9c71aa98ece5d77c5bcf6cfb00f829119af490
Jaehyuk Heo <wogur379@gmail.com>
2021-04-23 14:21:09 +0900

    Update run_translate.sh

--- SQLova/run_translate.sh
+++ SQLova/run_translate.sh
@@ -1,15 +1,15 @@
 # ===============
-# header 번역 x
+# ko_token: header 번역 x
 # ===============
 python translate.py --replace value --savedir ./data/ko_token --header
 python translate.py --replace token --savedir ./data/ko_token --header
 # ===============
-# header 번역 
+# ko_token_not_h: header 번역 
 # ===============
 python translate.py --replace value --savedir ./data/ko_token_not_h
 python translate.py --replace token --savedir ./data/ko_token_not_h
 # ===============
-# from table 
+# ko_from_table: from table 
 # ===============
 python translate.py --replace value --from_table --savedir ./data/ko_from_table
 python translate.py --replace token --from_table  --savedir ./data/ko_from_table
