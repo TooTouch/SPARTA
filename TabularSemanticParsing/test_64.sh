4e424a5574113910d4e7e1c825ece48093d1418c
hoonst <yoonhs1993@nate.com>
2021-05-21 04:39:50 +0000

    train_script

---
+++ TabularSemanticParsing/test_64.sh
@@ -0,0 +1,4 @@
+./experiment-bridge.sh configs/bridge/wikisql-bridge-bert-large.sh --inference 0 --test --data_dir data/ko_token --db_dir data/ko_token --beam_size 64
+./experiment-bridge.sh configs/bridge/wikisql-bridge-bert-large.sh --inference 0 --test --data_dir data/ko_token_not_h --db_dir data/ko_token_not_h --beam_size 64
+./experiment-bridge.sh configs/bridge/wikisql-bridge-bert-large.sh --inference 0 --test --data_dir data/ko_from_table --db_dir data/ko_from_table --beam_size 64
+./experiment-bridge.sh configs/bridge/wikisql-bridge-bert-large.sh --inference 0 --test --data_dir data/ko_from_table_not_h --db_dir data/ko_from_table_not_h --beam_size 64
