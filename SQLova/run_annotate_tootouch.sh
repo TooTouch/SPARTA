53f5a42f3dc893a639a0920b2f29d8b176452b1f
TooTouch <wogur379@gmail.com>
2021-05-27 13:57:55 +0900

    update path

--- SQLova/run_annotate_tootouch.sh
+++ SQLova/run_annotate_tootouch.sh
@@ -1,6 +1,4 @@
-python annotate_tootouch.py --din ./data/ko_token --dout ./data/ko_token
-python annotate_tootouch.py --din ./data/ko_token_not_h --dout ./data/ko_token_not_h
-python annotate_tootouch.py --din ./data/ko_from_table --dout ./data/ko_from_table
-python annotate_tootouch.py --din ./data/ko_from_table_not_h --dout ./data/ko_from_table_not_h
-
-
+python annotate_tootouch.py --din ../data/ko_token --dout ../data/ko_token
+python annotate_tootouch.py --din ../data/ko_token_not_h --dout ../data/ko_token_not_h
+python annotate_tootouch.py --din ../data/ko_from_table --dout ../data/ko_from_table
+python annotate_tootouch.py --din ../data/ko_from_table_not_h --dout ../data/ko_from_table_not_h
--- SQLova/run_train.sh
+++ SQLova/run_train.sh
@@ -2,7 +2,7 @@
 # wikisql raw
 # ====================
 python3 train.py --do_train --seed 1 --bS 8 --tepoch 10 \
-                 --datadir ../data/data --logdir ./logs/sqlova_data \
+                 --datadir .../data/data --logdir ./logs/sqlova_data \
                  --accumulate_gradients 2 --bert_name bert-base-uncased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1
@@ -11,12 +11,12 @@ python3 train.py --do_train --seed 1 --bS 8 --tepoch 10 \
 # ko_token 
 # ====================
 python3 train.py --do_train --seed 1 --bS 8 --tepoch 50 \
-                 --datadir ./data/ko_token --logdir ./logs/ko_token \
+                 --datadir ../data/ko_token --logdir ./logs/ko_token \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG
 
 python3 train.py --do_train --seed 1 --bS 8 --tepoch 50 \
-                 --datadir ./data/ko_token --logdir ./logs/ko_token \
+                 --datadir ../data/ko_token --logdir ./logs/ko_token \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --trained
@@ -26,18 +26,18 @@ python3 train.py --do_train --seed 1 --bS 8 --tepoch 50 \
 # ko_token_not_h
 # ====================
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 50 \
-                 --datadir ./data/ko_token_not_h --logdir ./logs/ko_token_not_h \
+                 --datadir ../data/ko_token_not_h --logdir ./logs/ko_token_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG
 
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
-                 --datadir ./data/ko_token_not_h --logdir ./logs/ko_token_not_h \
+                 --datadir ../data/ko_token_not_h --logdir ./logs/ko_token_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --trained
 
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
-                 --datadir ./data/ko_token_not_h --logdir ./logs/ko_token_not_h \
+                 --datadir ../data/ko_token_not_h --logdir ./logs/ko_token_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --trained
@@ -47,13 +47,13 @@ python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
 # ko_from_table
 # ====================
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 50 \
-                 --datadir ./data/ko_from_table --logdir ./logs/ko_from_table \
+                 --datadir ../data/ko_from_table --logdir ./logs/ko_from_table \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1
 
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
-                 --datadir ./data/ko_from_table --logdir ./logs/ko_from_table \
+                 --datadir ../data/ko_from_table --logdir ./logs/ko_from_table \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1 --trained
@@ -62,19 +62,19 @@ python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
 # ko_from_table_not_h
 # ====================
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 50 \
-                 --datadir ./data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
+                 --datadir ../data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1
 
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
-                 --datadir ./data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
+                 --datadir ../data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1 --trained
 
 python3 train.py --do_train --seed 223 --bS 8 --tepoch 20 \
-                 --datadir ./data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
+                 --datadir ../data/ko_from_table_not_h --logdir ./logs/ko_from_table_not_h \
                  --accumulate_gradients 2 --bert_name bert-base-multilingual-cased \
                  --fine_tune --lr 0.001 --lr_bert 0.00001 --max_seq_length 222 --EG \
                  --gpu 1 --trained
